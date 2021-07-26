0116672d3a6ca5c3a592d0fe69d99471daed4c29
porjaii26 <rattanon_p@silpakorn.edu>
2021-07-26 19:17:56 +0700

    react-create-app

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -9,7 +9,7 @@ class Home extends Component{
                 <div className="condiv home">
                 <img src={[profile1]} speed={100} eraseDelay={2500} alt="profile" className="profilepic" />
                 <ReactTypingEffect text={['Wellcom','I am Porruthai Rattanon']} speed={100} eraseDelay={2500} className="typingeffect"></ReactTypingEffect>
-                <Social />
+              
             </div>
 
             </div>
